e58f9e7893ffa10b8fe58c3e7efc724c49ea252c
Gergo Konkoly <95075895+whowful@users.noreply.github.com>
2024-01-16 09:29:34 +0100

    add basic functions for battle.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Battle from './components/Battle'
 
 function App() {
   return (
     <>
+      <Battle playerPokemon={'https://pokeapi.co/api/v2/pokemon/charizard'} enemyPokemon={'https://pokeapi.co/api/v2/pokemon/charizard'}/>
     </>
   )
 }
--- src/components/battle.jsx
+++ src/components/battle.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react"
+import { useEffect } from "react"
 
-function battle() {
+function Battle(props) {
+  const [pokemon, setPokemon] = useState(null)
+  const [level, setLevel] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(props.playerPokemon)
+      const fetchedPokemon = await response.json()
+      setPokemon(fetchedPokemon)
+      console.log(fetchedPokemon)
+      addLevelToPokemon()
+    }
+    fetchData()
+  }, [props.playerPokemon])
+
+  const addLevelToPokemon = () => {
+    const randomLevel = Math.ceil(Math.random() * 100)
+    console.log(randomLevel)
+    setLevel(randomLevel)
+  }
+
   return (
-    <div>battle</div>
+    <>
+      <div>
+        {
+        pokemon &&
+        `Pokemon: ${pokemon.name} -
+        Type: ${pokemon.types.map(type => type.type.name).join(', ')} -
+        Level: ${level} - 
+        HP: ${pokemon.stats[0]['base_stat']}
+        `
+        }
+      </div>
+    </>
   )
 }
 
-export default battle
+export default Battle
